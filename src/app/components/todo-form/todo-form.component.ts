6a81a7c21378d28e0d61187c963334f141eed375
Ninad Vadujkar <nv_vit@yahoo.in>
2018-08-24 02:34:32 +0530

    Make todo form reactive.
    -- Add appropriate directives to the form to make it reactive
    -- Create form group in component class
    -- Form very similar to the one used in login
    -- Wire onAdd method to form submission. Does not do anything yet.

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-form.component.scss']
 })
 export class TodoFormComponent implements OnInit {
-
-  constructor() { }
+  todoForm: FormGroup;
+  formData: any;
+  formErrors: any = {
+    'todo': '',
+  };
+  validationMessages: any = {
+    'todo': {
+      'required': 'Todo is required',
+    },
+  };
+  todoCreateErrMsg: string;
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.todoForm = this._fb.group({
+      todo: ['', [Validators.required]],
+    });
+
+    this.todoForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if(!this.todoForm) return;
+    const form = this.todoForm;
+
+    for(let field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if(control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for(let key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  resetForm(): void {
+    this.todoCreateErrMsg = '';
+    this.todoForm.reset({
+      username: '',
+      password: '',
+    });    
+  }
+
+  onAdd(): void {
+    console.log(this.todoForm.get('todo').value);
   }
 
 }
